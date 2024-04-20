4175db532c6d34cec3fc6e10ff030f9a42c7da1e
nathaliasal <nathaliasalguero@gmail.com>
2024-04-19 23:18:20 -0500

    Se agregó la fuente

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,14 @@ export default {
   ],
   theme: {
     extend: {
-      'Azul-oscuro': '#0081A7',
-      'Azul-claro': '#00AFB9',
-      'Beige': '#FDFCDC',
-      'Salmon': '#FED9B7',
-      'Rojo': '#F07167',
+      colors: {
+        'Azul-oscuro': '#0081A7',
+        'Azul-claro': '#00AFB9',
+        'Beige': '#FDFCDC',
+        'Salmon': '#FED9B7',
+        'Rojo': '#F07167',
+      }
+
     },
   },
   plugins: [],
