93b7103e648e812bf66fb95dd011dfecca4c182c
Davfyx07 <davfy07@gmail.com>
2024-04-24 16:34:57 -0500

    FalshCard
    productos

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Flashcard from './components/Flashcard'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
     <>
       <p>
         Bienvenido a mi piñatería :D
+        <Flashcard/>
       </p>
     </>
   )
