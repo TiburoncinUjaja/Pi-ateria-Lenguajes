84bd73eca1b7fd32b73fd20bb0e4e7f964e783e6
TiburoncinUjaja <gogeta1092@hotmail.com>
2024-04-28 13:50:45 -0500

    Sistema de registro con guardado local

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import FormRegister from '../components/FormRegister'
+import RegistroList from '../components/RegisterList'
 
 const Login = () => {
   return (
     <div>
     <FormRegister/>
+    <RegistroList />
     </div>
     
   )
