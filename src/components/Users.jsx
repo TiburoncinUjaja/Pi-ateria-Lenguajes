ac12f7d0f76143bee17b85ea009c231229d900ab
JuanDLara <juandavid.lara.20@gmail.com>
2024-04-30 17:48:15 -0500

    Version1.0 Hola pepita

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -7,6 +7,12 @@ import { FaUserAstronaut } from "react-icons/fa";
 
 const Users = () => {
   const [isOpen, setIsOpen] = useState(false);
+  
+  function obtenerNombre(){
+
+    const sesion = JSON.parse(localStorage.getItem('sesion')) || [];
+    return sesion ? sesion[0] : null
+  }
 
   const handleToggle = () => {
     setIsOpen(!isOpen);
@@ -14,17 +20,19 @@ const Users = () => {
 
   const handleLogout = () => {
     // Aquí añadir la lógica para cerrar la sesión
+    localStorage.setItem('sesion', JSON.stringify(null));
+    window.location.href = '/';
     alert('¡Sesión cerrada!');
   };
   return (
     <div className="max-w-7xl mx-auto px-4 flex justify-between divide-x divide-Azul-claro">
-      <Link to="/carrito" className='align-middle'>
-        <FontAwesomeIcon className='px-5 py-3' icon={faCartShopping} style={{ color: "#0081A7"}} />
+      <Link to="/carrito" className='align-middle flex items-center justify-center'>
+        <FontAwesomeIcon className='px-5 py-3' icon={faCartShopping} style={{ color: "#0081A7" }} />
       </Link>
-      <ul className="flex items-center space-x-4 px-3">
+      <ul className="flex items-center space-x-4 px-6">
         <li className="relative">
-          <button className="w-full shadow-lg rounded-br-lg p-3 inline-block bg-gray-100 h-full" onClick={handleToggle}>
-          Hola, gordita
+          <button className="w-full shadow-lg rounded-br-lg p-3 inline-block bg-gray-100 flex items-center justify-center gap-x-2" onClick={handleToggle}>
+            <FaUserAstronaut className=''/>Hola, {obtenerNombre()}
           </button>
           {isOpen && (
             <ul className="absolute right-0 mt-2 w-48 bg-white border border-gray-200 rounded-lg shadow-md divide-y divide-gray-100">
