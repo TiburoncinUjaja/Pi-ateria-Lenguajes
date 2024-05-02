29c8f277c7796354e847f38533be8e087bdc0c4f
Davfyx07 <davfy07@gmail.com>
2024-05-02 14:30:31 -0500

    FormContact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
+import FormContact from '../components/FormContact'
 
 
 const Contact = () => {
   return (
-    <section className='body font relative text-gray-600 '>
-        <div className='container px-5 py-20 mx-auto'>
-            <div className='felx flex-col text-center w-full mb-12 '>
-                <h1 className='sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900 text-left'>¡Contactenos!</h1>
-            </div>
-        </div>
-       
-    </section>
+    <div>
+      <FormContact/>
+    </div>
   )
 }
 
